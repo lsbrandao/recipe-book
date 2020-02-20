6eafa98849d806b013ccf51932a63533902cfce4
Luiz Brandao <luiz.sb@live.com>
2020-02-19 22:55:00 -0500

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "This is simply a test",
+      "https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg"
+    ),
+    new Recipe(
+      "A test recipe",
+      "This is simply a test",
+      "https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg"
+    )
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
